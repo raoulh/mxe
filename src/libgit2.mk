0f97aa144e0d28ef3842cad4ad5923e6a075857a
Raoul Hecky <r.hecky@emc-partner.ch>
2016-09-22 09:55:50 +0200

    update libgit

--- src/libgit2.mk
+++ src/libgit2.mk
@@ -3,8 +3,8 @@
 
 PKG             := libgit2
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.23.2
-$(PKG)_CHECKSUM := 20c0a6ee92c0e19207dac6ddc336b4ae4a1c4ddf91be0891e4b6e6ccba16df0b
+$(PKG)_VERSION  := 0.24.1
+$(PKG)_CHECKSUM := 60198cbb34066b9b5c1613d15c0479f6cd25f4aef42f7ec515cd1cc13a77fede
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/libgit2/libgit2/archive/v$($(PKG)_VERSION).tar.gz
